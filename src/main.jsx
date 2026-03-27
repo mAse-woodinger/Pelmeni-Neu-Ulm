6f24fb137d9d3f62e636309a51dec3ee1049c49b
mAse-woodinger <matthias.holzinger@gmx.net>
2026-03-27 21:53:37 +0100

    Initialize React app and import global styles in main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/global.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to the React App!</h1>
+        </div>
+    );
+}
+
+export default App;
